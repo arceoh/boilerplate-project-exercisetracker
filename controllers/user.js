ba30686e29441eb0c7f9e2564b7bbb81dfb0ce53
Hugo <arceoh@gmail.com>
2023-09-06 11:15:05 -0700

    Create user

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,25 @@ const asyncHandler = require("express-async-handler");
 const { User } = require("../models/userModel");
 
 const createUser = asyncHandler(async (req, res) => {
-  res.json({ message: "User Created" });
+  const { username } = req.body;
+
+  if (!username) {
+    res.status(400);
+    throw new Error("Username is required");
+  }
+
+  const existingUser = await User.findOne({ username: username });
+
+  if (existingUser) {
+    res.json({ username: existingUser.username, _id: existingUser._id });
+  } else {
+    // Create new user
+    const newUser = await User.create({
+      username: username,
+    });
+    await newUser.save();
+    res.json({ username: newUser.username, _id: newUser._id });
+  }
 });
 
 const getUser = asyncHandler(async (req, res) => {
