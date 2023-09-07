d3f94d92e483e5ffe2e46ade64b8bd3c0ac0e439
Hugo <arceoh@gmail.com>
2023-09-07 08:02:52 -0700

    Schema validation

--- models/exerciseModel.js
+++ models/exerciseModel.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const exerciseSchema = mongoose.Schema(
   {
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    description: String,
-    duration: Number,
-    date: Date,
+    description: { type: String, required: true },
+    duration: { type: Number, required: true },
+    date: { type: Date, required: false },
   },
   { timestamps: true }
 );
